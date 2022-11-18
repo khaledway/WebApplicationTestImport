ab978312be391311d2b40126c6c77712f1e2d2f6
khaled hussein <khaled.hussein@57357.org>
2022-11-18 16:52:28 +0200

    add new page and javsscript

---
+++ WebApplicationTestImport/ImportSheet.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ImportSheet.aspx.cs" Inherits="WebApplicationTestImport.ImportSheet" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://cdn.kendostatic.com/2014.3.1119/styles/kendo.common.min.css">
+    <link rel="stylesheet" href="https://cdn.kendostatic.com/2014.3.1119/styles/kendo.rtl.min.css">
+    <link rel="stylesheet" href="https://cdn.kendostatic.com/2014.3.1119/styles/kendo.default.min.css">
+    <link rel="stylesheet" href="https://cdn.kendostatic.com/2014.3.1119/styles/kendo.dataviz.min.css">
+    <link rel="stylesheet" href="https://cdn.kendostatic.com/2014.3.1119/styles/kendo.dataviz.default.min.css">
+    <link rel="stylesheet" href="https://cdn.kendostatic.com/2014.3.1119/styles/kendo.mobile.all.min.css">
+
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="https://cdn.kendostatic.com/2014.3.1119/js/angular.min.js"></script>
+    <script src="https://cdn.kendostatic.com/2014.3.1119/js/jszip.min.js"></script>
+    <script src="https://cdn.kendostatic.com/2014.3.1119/js/kendo.all.min.js"></script>
+</head>
+<body>
+<%--    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>--%>
+
+    <input id="txtSheetID" type="text" placeholder="enter sheet ID" />
+    <input id="txtSheetName" type="text" placeholder="enter sheet Name" />
+    <input id="btnDisplayData" type="button" value="button"  onclick="init();"/>
+    <script src="Sheet.js"></script>
+
+    <div id="grid"></div>
+</body>
+</html>
---
+++ WebApplicationTestImport/Sheet.js
@@ -0,0 +1,127 @@
+﻿
+function init()
+{
+
+    //$("#grid").data("kendoGrid").dataSource.data([]);
+
+    $("#grid").kendoGrid({
+        dataSource: [],
+        columns: [],
+    })
+
+    //const sheetID = '1Unojl6HwNgjofDEc8-WzTINLDmBn2xwyQeYCZtM3T68';
+    //const base = 'https://docs.google.com/spreadsheets/d/' + sheetID + '/gviz/tq?';
+    //const sheetName = "Assets";
+    //const query = encodeURIComponent('Select *');
+    //const url = base + 'sheet=' + sheetName + '&tq=' + query;
+
+    const sheetID = document.getElementById("txtSheetID").value
+
+    //1Unojl6HwNgjofDEc8-WzTINLDmBn2xwyQeYCZtM3T68
+    const base = 'https://docs.google.com/spreadsheets/d/' + sheetID + '/gviz/tq?';
+    const sheetName = document.getElementById("txtSheetName").value//"Assets";
+    const query = encodeURIComponent('Select *');
+    const url = base + 'sheet=' + sheetName + '&tq=' + query;
+
+
+
+
+
+
+    
+    console.log('ready');
+    console.log(url);
+    fetch(url)
+    .then(res=>
+    
+        //debugger;
+        //res.text();
+        //console.log(res.status);
+        res.text()
+
+    )
+    .then(rep=>
+    {
+
+       // console.log(rep);
+        debugger;
+
+        const jsData = JSON.parse(rep.substr(47).slice(0, -2));
+        //console.log(jsData);
+        const colz = [];
+        const data = [];
+        jsData.table.cols.forEach(heading=> {
+            //console.log(heading);
+
+            if (heading.label) {
+                const propName = heading.label.toLowerCase().
+                replace(/\s/g, '');
+                colz.push(propName);
+                //console.log(colz);
+            }
+
+        })
+
+
+        jsData.table.rows.forEach((main) => {
+            // console.log(main);
+            const row = {};
+            colz.forEach((ele, ind) => {
+
+                //console.log(ele);
+                row[ele] = (main.c[ind] != null ? main.c[ind].v : '');
+            })
+            data.push(row);
+        })
+
+       // console.log(data);
+
+        PupulateToKindo(colz, data);
+
+    }).catch(err =>
+    {
+        console.log("error", err);
+        alert("invalid sheet id or name");
+        document.getElementById("txtSheetID").value = '';
+        document.getElementById("txtSheetName").value = '';
+
+    });
+}
+
+
+
+
+function PupulateToKindo(colz, dataRows) {
+    
+
+    BindColumns(colz);
+    $("#grid").kendoGrid({
+        dataSource: {
+            data:
+                {
+                    "items": dataRows,
+                },
+            schema:
+                {
+                    data: "items"
+                }
+        },
+        columns: BindColumns(colz),
+
+  
+
+    })
+}
+
+				
+
+
+function BindColumns(colz)
+{
+const colzArr = [];
+    colz.forEach(s=>
+    { 
+        colzArr.push({ "field": s, "title": s.toUpperCase() });
+    });
+    return colzArr; 
+}
